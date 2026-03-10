f6bcca78f7405971ca008b023a2e0f1b41014cc2
Ritesh-ScriptHub <vritesh90833@gmail.com>
2026-03-10 15:26:59 +0530

    added navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
+import NavBar from './components/NavBar'
 import Favourites from './pages/Favourites'
 import Home from './pages/Home'
 import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar/>
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
