93ba7674c98b5e1aac44c2dfcc687ffbd3f829b8
Ritesh-ScriptHub <vritesh90833@gmail.com>
2026-03-11 18:18:28 +0530

    added api.js

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+const BASE_URL = "http://localhost:5000/api"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/popular`)
+    const data = await response.json()
+    return data
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search?q=${encodeURIComponent(query)}`)
+    const data = await response.json();
+    return data;
+}
